ca7bec2ef29a7d9fd8b8fddf60202f38fc4be242
Trieu <trieu186168@gmail.com>
2023-12-28 16:07:31 +0700

    add

--- main.js
+++ main.js
@@ -159,13 +159,12 @@ function soSanhDuongAm(arr) {
 }
 
 function isPrime(num) {
-  if (num < 2) {
-    return false;
-  }
+  
   for (var i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
     }
   }
+
   return true;
 }
